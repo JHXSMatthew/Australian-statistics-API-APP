c469188e7607f5d4a331fc16df9da45715a5d031
JHXSMatthew <68638023@qq.com>
2017-03-23 15:11:01 +1100

    ready to deploy

--- src/main/java/com/teamrocket/seng3011/AustralianStatistics.java
+++ src/main/java/com/teamrocket/seng3011/AustralianStatistics.java
@@ -2,9 +2,10 @@ package com.teamrocket.seng3011;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AustralianStatistics {
+public class AustralianStatistics extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AustralianStatistics.class, args);

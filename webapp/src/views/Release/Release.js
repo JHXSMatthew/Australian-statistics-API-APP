5cfddad640d62e25b1666e14f7e9ebfd8249cb16
JHXSMatthew <68638023@qq.com>
2017-04-10 22:53:44 +1000

    realease entries fetch dynamically.

--- webapp/src/views/Release/Release.js
+++ webapp/src/views/Release/Release.js
@@ -1,39 +1,118 @@
 import React, { Component } from 'react';
 
-
 class Release extends Component {
 
   constructor() {
      super();
      this.state = null;
+     var that = this;
+     var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+     var url = 'http://45.76.114.158/data/release.json'
+     fetch(proxyUrl + url)
+     .then(function(response) {
+       if (response.status >= 400) {
+         throw new Error("Bad response from server");
+       }
+       return response.json().then(function(json) {
+         that.setState(
+           {
+             data: json
+           }
+         );
+       });
+     });
    }
 
-  componentDidMount() {
-    var that = this;
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    var url = 'http://45.76.114.158/data?type=release.json'
-
-    fetch(proxyUrl + url)
-    .then(function(response) {
-      if (response.status >= 400) {
-        throw new Error("Bad response from server");
-      }
-      that.setState = response.json();
-      console.log(response);
-    })
-    .then(function(data) {
-      that.setState({ person: data.person });
-    });
-  }
-
-
   render() {
+    var cards = [];
+    if(this.state && this.state.data){
+      for(var i = 0; i < this.state.data.length ; i ++){
+        cards.push(getCard(this.state.data[i]));
+      }
+
+    }else{
+      cards = null;
+    }
+
+
+
     return (
+      <div className="animated fadeIn">
         <div className="row">
-            this.state
+            {cards}
         </div>
+      </div>
     )
   }
+
+
+  /*
+    constructor(){
+      super();
+      this.state = {
+        data: null
+      };
+      var that = this;
+      var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+      var url = 'http://45.76.114.158/data/release.html'
+      fetch(proxyUrl + url)
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json().then(function(text) {
+          that.setState({data: text});
+        });
+      });
+    }
+
+
+    render() {
+      return(
+        <div className="animated fadeIn">
+          <div className="row">
+              <div dangerouslySetInnerHTML={{__html: this.state.data}}></div>
+          </div>
+        </div>
+      )
+    }
+  */
+
 }
+
+
+function getCard(cardRep){
+  return <div className="col-sm-6 col-md-4">
+            <div className="card">
+            {getHeader(cardRep.title,cardRep.date)}
+            <div className="card-block">
+              {getText(cardRep.contenet)}
+              <p> Bug Fixes </p>
+              {getDot(cardRep.fix)}
+              <p> New Features </p>
+              {getDot(cardRep.new)}
+            </div>
+          </div>
+        </div>
+}
+
+function getHeader(title,date){
+  return <div className="card-header"> {title}  <small class="text-muted">{forceHTML(date)}</small> </div>;
+}
+
+function getText(t){
+  return <div> {t.map((a) => <p> {forceHTML(a)} </p>)} </div>;
+}
+
+function getDot(t){
+  return <ul> {t.map((b) => <li key={b} > {forceHTML(b)}</li>)} </ul>;
+}
+
+function forceHTML(t){
+  return <div dangerouslySetInnerHTML={{__html: t}} />
+}
+
+
+
 
 export default Release;

c0bb0a23fb893a18e992722b7e9f0996a6fc4e83
Suvercha Khattar <z5062664@student.unsw.edu.au>
2017-04-13 00:28:19 +1000

    made changes to getting started

--- webapp/src/views/Statistics/Doc/GettingStarted/GettingStarted.js
+++ webapp/src/views/Statistics/Doc/GettingStarted/GettingStarted.js
@@ -7,16 +7,15 @@ class GettingStarted extends Component {
       <div className="animated fadeIn">
         <div className="row">
             <div className="card">
-              <div className="card-header">
-                Introduction
-              </div>
               <div className="card-block">
+              <h1> Australian Statistics API </h1>
                 We have developed an Australian Statistics API that will receive a request from any third party, with specifications
-                on various filters such as area of statistics, regions, list of categories (industried or commodities) and a time 
-                period. Our API then returns the relevant statistics to the user. 
+                on various filters such as area of statistics, regions, list of categories (industries or commodities) and a time
+                period. Our API then returns the relevant statistics to the user.
                 <br/><br/>
+                <a className="btn btn-primary btn-lg" href="#/dataAnalyzer" role="button">Try It Out</a><br/><br/>
                 Users are able to call our API using HTTP request methods. In this case, our API will return the relevant statistical
-                results back through a JSON format. <br/><a href="#/about">Click here to view our developer API documentation.</a>
+                results back in a JSON format. <br/><a href="#/about">Click here to view our developer API documentation.</a>
                 <br/><br/>
                 Our website also offers an interface to test various calls to our API, for demonstration purposes.
                 <br/><a href="#/about">Click here to view our Data Analyser documentation.</a>

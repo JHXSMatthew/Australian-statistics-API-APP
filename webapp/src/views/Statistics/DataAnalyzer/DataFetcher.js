766ca415a0d8972fcf8b123c8cb60b8b55454e47
JHXSMatthew <68638023@qq.com>
2017-05-05 06:28:49 +1000

    more orgnized code.

---
+++ webapp/src/views/Statistics/DataAnalyzer/DataFetcher.js
@@ -0,0 +1,265 @@
+//data fetcher components
+import React, {Component} from 'react';
+import Picker from 'react-month-picker';
+import Select from 'react-select';
+import { PopoverTitle, PopoverContent,Popover} from 'reactstrap';
+import {AREA} from './DataAnalyzer.js';
+import {STATE} from './DataAnalyzer.js';
+import {CATEGORY_RT} from './DataAnalyzer.js';
+import {CATEGORY_ME} from './DataAnalyzer.js';
+
+
+const DATE_LANG = {
+       months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+       , from: 'From', to: 'To'
+}
+
+class DataFetcher extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      area: null,
+      region: [],
+      category: [],
+      mrange: {from: {year: 2016, month: 7}, to: {year: 2017, month: 2}},
+      count: 0,
+      popoverOpen: false
+    }
+    this.setArea = this.setArea.bind(this);
+    this.setCategory = this.setCategory.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.setRegion = this.setRegion.bind(this);
+    this.fetch = this.fetch.bind(this);
+    this.handleClickRangeBox = this.handleClickRangeBox.bind(this);
+    this.handleRangeDissmis = this.handleRangeDissmis.bind(this);
+  }
+
+  toggle() {
+      this.setState({
+        popoverOpen: !this.state.popoverOpen
+      });
+    }
+
+  setArea(a){
+    this.setState({
+      area: a,
+      category: []
+    })
+  }
+
+  setRegion(a){
+    this.setState({
+      region: a,
+    })
+  }
+
+  setCategory(a){
+    this.setState({
+      category: a
+    })
+  }
+
+  handleClickRangeBox(e) {
+      this.refs.pickRange.show()
+  }
+
+  handleRangeDissmis(value) {
+    this.setState( {mrange: value} )
+  }
+
+  fetch(e){
+    var that = this;
+    var url = 'http://45.76.114.158/api/'
+    fetch( url,{
+      method: 'POST',
+      headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        StatisticsArea: this.state.area,
+        State: this.state.region,
+        Category: this.state.category,
+        startDate: this.state.mrange.from.year + "-" + this.state.mrange.from.month + "-01",
+        endDate: this.state.mrange.to.year + "-" + this.state.mrange.to.month + "-01"
+      }),
+    })
+    .then(function(response) {
+      if (response.status >= 400) {
+        alert("Our data source is down, please wait for a while and we'll fix it asap.")
+        return;
+      }
+      return response.json().then(function (json) {
+        //TODO: deal with empty data
+        that.props.addDataEntry(json,"Data Entry"+that.state.count++);
+      })
+
+    });
+  }
+
+  render(){
+    var button = (this.state.area && this.state.region && this.state.category && this.state.region.length > 0 && this.state.category.length > 0) ? false : true;
+
+    return (
+        <div className="card">
+          <div className="card-header">
+            <strong>Data fetcher</strong>
+            <span className="float-right"><i className="icon-question" id="Popover1" onClick={this.toggle}></i></span>
+          </div>
+          <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
+             <PopoverTitle>Data Fetcher</PopoverTitle>
+             <PopoverContent>Fill all forms and click fetch to fetch data. All fetched data will be shown in Data Set and charts will be shown.</PopoverContent>
+           </Popover>
+          <div className="card-block">
+            <div className="row">
+              <div className="col-sm-12 col-md-12">
+                <div className="form-group">
+                  <label htmlFor="name">Statistics Area</label>
+                  <AreaSelector setArea={this.setArea}/>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-12 col-md-12">
+                <div className="form-group">
+                  <label htmlFor="regions">Regions</label>
+                  <StateSelector setRegion={this.setRegion}/>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-12 col-md-12">
+                <div className="form-group">
+                  <label htmlFor="category">Category</label>
+                  <CategorySelector setCategory={this.setCategory} area={this.state.area} category={this.state.category}/>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-12 col-md-12">
+                <div className="form-group">
+                  <label htmlFor="daterange">Date Range</label>
+                  <MonthBox value={this.state.mrange} ClickRangeBox={this.handleClickRangeBox} />
+                </div>
+              </div>
+            </div>
+
+              <Picker
+                  ref="pickRange"
+                  years={{min: 1995, max: 2017}}
+                  range={this.state.mrange}
+                  lang={DATE_LANG}
+                  theme="light"
+                  onChange={this.handleRangeChange}
+                  onDismiss={this.handleRangeDissmis}
+                  >
+              </Picker>
+          </div>
+          <div className="card-footer">
+            <button className="btn btn-sm btn-primary" onClick={this.fetch} disabled={button} ><i className="fa fa-dot-circle-o" ></i> Fetch</button>
+          </div>
+
+        </div>
+
+    )
+  }
+}
+
+
+class Switch_Text extends Component{
+
+  render(){
+    return (
+      <label className="switch switch-3d switch-primary">
+        <input type="checkbox" className="switch-input" defaultChecked/>
+        <span className="switch-label"></span>
+        <span className="switch-handle"></span>
+      </label>
+    )
+  }
+
+}
+
+class MonthBox extends Component {
+  constructor(props){
+      super(props);
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.props.ClickRangeBox();
+  }
+
+  render(){
+    let makeText = m => {
+        if (m && m.year && m.month) return (DATE_LANG.months[m.month-1] + '. ' + m.year)
+        return '?'
+    }
+    return <div className="a" onClick={this.handleClick}>
+              <button type="button" className="btn btn-outline-primary btn-block"><b>{makeText(this.props.value.from) + ' ~ ' + makeText(this.props.value.to)}</b></button>
+            </div>
+  }
+
+}
+
+
+class AreaSelector extends Component{
+  constructor(prop){
+    super(prop)
+    this.state = {
+      disabled: false,
+      options: AREA,
+      value: '',
+    };
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleSelectChange(value) {
+  		this.setState({value});
+      this.props.setArea(value);
+  }
+
+  render(){
+    return <Select simpleValue value={this.state.value} placeholder="Select Statistics Area" options={this.state.options} onChange={this.handleSelectChange} />
+  }
+}
+
+class StateSelector extends Component{
+  constructor(prop){
+    super(prop)
+    this.state = {
+      disabled: false,
+      options: STATE,
+      value: [],
+    };
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleSelectChange(value) {
+  		this.setState({ value });
+      this.props.setRegion(value);
+  }
+
+  render(){
+    return <Select multi simpleValue value={this.state.value} placeholder="Select regions" options={this.state.options} onChange={this.handleSelectChange} />
+  }
+}
+
+
+class CategorySelector extends Component{
+  constructor(props){
+    super(props)
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleSelectChange(value) {
+      this.props.setCategory(value);
+  }
+
+  render(){
+    return <Select multi simpleValue disabled={(this.props.area) ? false : true} value={this.props.category} placeholder="Select Categories" options={this.props.area &&this.props.area === "Retail" ? CATEGORY_RT : CATEGORY_ME } onChange={this.handleSelectChange} />
+  }
+}
+
+export default DataFetcher;
---
+++ webapp/src/views/Statistics/DataAnalyzer/DataTable.js
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+import { PopoverTitle, PopoverContent,Popover,Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import ReactTable from 'react-table';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+
+
+class DataTable extends Component {
+  constructor(props){
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.toggle2 = this.toggle2.bind(this);
+    this.state ={
+      rawShowing: false,
+      popoverOpen: false
+    }
+  }
+
+  toggle() {
+    this.setState({
+      rawShowing: !this.state.rawShowing
+    });
+  }
+
+  toggle2() {
+  this.setState({
+    popoverOpen: !this.state.popoverOpen
+  });
+}
+
+  render() {
+    var data = this.props.data;
+    const categoryValue = [{
+      header: 'Category',
+      accessor: 'Category' // String-based value accessors!
+    }, {
+      header: 'Average',
+      accessor: 'average',
+    }]
+
+    const regionalData = [{
+      header: 'State',
+      accessor: 'State' // String-based value accessors!
+    }, {
+      header: 'Average',
+      accessor: 'average',
+    }]
+
+    const Datedata = [{
+      header: 'Date',
+      accessor: 'Date' // String-based value accessors!
+    }, {
+      header: 'Value',
+      accessor: 'Value',
+    }]
+    var unit = null;
+    if(this.props.dataType === "Export"){
+      unit = "($ thousands)"
+    }else if(this.props.dataType === "Retail"){
+      unit = "($ millions)";
+    }
+
+    return(
+      <div className="card">
+        <div className="card-header">
+          <strong>Data Set {unit} <span className="float-right"><i className="icon-question" id="Popover2" onClick={this.toggle2}></i></span></strong>
+        </div>
+        <Popover placement="left" isOpen={this.state.popoverOpen} target="Popover2" toggle={this.toggle2}>
+         <PopoverTitle>Data Set</PopoverTitle>
+         <PopoverContent>This table will initially list categories and display their corresponding average values fetched by the data fetcher. The table can expand the data set with the arrow on the left of the rows to show the averages of the value per state requested. it can further expand to display individual values for the months requested. </PopoverContent>
+       </Popover>
+        <div className="card-block">
+          <div className="row">
+            <div className="col-sm-12 col-md-12">
+              <ReactTable
+                data={data}
+                columns={categoryValue}
+                defaultPageSize={5}
+                noDataText='Use Data Fetcher to fetch data.'
+                pageSize={(data &&  data.length) ? data.length : 7}
+                SubComponent={(row) => {
+                  return(
+                    <ReactTable
+                      data={row.row.RegionalData}
+                      columns={regionalData}
+                      defaultPageSize={10}
+                      pageSize={row.row.RegionalData.length}
+                      showPagination={false}
+                      SubComponent={(row) => {
+                        return(
+                          <ReactTable
+                            data={row.row.Data}
+                            pageSize={row.row.Data.length}
+                            columns={Datedata}
+                            defaultPageSize={10}
+                            showPagination={false}
+                          />
+                        )
+                      }}
+                    />
+                  )
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-sm btn-primary" onClick={this.toggle} disabled={(this.props.dataType) == null? true : false} ><i className="fa fa-dot-circle-o" ></i> Raw</button>
+        </div>
+        <Modal isOpen={this.state.rawShowing} toggle={this.toggle} className={'modal-lg '+ this.props.className}>
+          <ModalHeader toggle={this.toggle}>JSON</ModalHeader>
+          <ModalBody>
+            Click Copy to copy raw JSON into your clipboard. Only support IE6 and modern browser.
+          </ModalBody>
+          <ModalFooter>
+            <CopyToClipboard text={(this.props.data)?JSON.stringify(this.props.data,null,2):""}>
+              <Button color="primary" onClick={this.toggle}>Copy</Button>
+            </CopyToClipboard>
+            <Button color="secondary" onClick={this.toggle}>Close</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+
+
+
+    )
+  }
+
+}
+
+export default DataTable;

2eb1b0aff51e2ef5a22b2eb32d928196e4478439
JHXSMatthew <68638023@qq.com>
2017-05-09 13:18:01 +1000

    specify md of dataTable

--- webapp/src/views/Statistics/DataAnalyzer/ResultPanel.js
+++ webapp/src/views/Statistics/DataAnalyzer/ResultPanel.js
@@ -13,7 +13,7 @@ class ResultPanel extends Component{
               <Col md="8" xs="8" >
                 <Charts data={this.props.data} dataType={this.props.dataType} addTimePoint={this.addTimePoint}/>
               </Col>
-              <Col xs={{ size: 'auto' }}>
+              <Col  md="4" xs={{ size: '4' }}>
                 <DataTable data={this.props.data} dataType={this.props.dataType}/>
               </Col>
             </Row>

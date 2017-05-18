4c2a4dc8bcc071c8075724edbe97146f2581c10e
JHXSMatthew <68638023@qq.com>
2017-05-18 21:03:17 +1000

    new UI

---
+++ webapp/src/views/Statistics/DataAnalyzer/ComparisonView.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+import DataTable from './DataTable.js';
+import ChartSet from './ChartSet.js';
+import 'rc-slider/assets/index.css';
+import { Row, Col,Container} from 'reactstrap';
+
+
+class ResultPanel extends Component{
+    render(){
+      return(
+          <Container fluid={true}>
+            <Row>
+              <Col md="12" xs="12" >
+                <ChartSet data={this.props.data} dataType={this.props.dataType} addTimePoint={this.addTimePoint}/>
+              </Col>
+            </Row>
+            <Row>
+              <Col  md="12" xs={{ size: '12' }}>
+                <DataTable data={this.props.data} dataType={this.props.dataType}/>
+              </Col>
+            </Row>
+          </Container>
+
+      )
+    }
+}
+
+
+export default ResultPanel;

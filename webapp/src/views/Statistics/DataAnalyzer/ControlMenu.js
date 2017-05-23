d41d2e3a255244c3c55bcc9a0373918b5528f43b
JHXSMatthew <68638023@qq.com>
2017-05-23 13:29:26 +1000

    design

--- webapp/src/views/Statistics/DataAnalyzer/ControlMenu.js
+++ webapp/src/views/Statistics/DataAnalyzer/ControlMenu.js
@@ -10,24 +10,20 @@ class ControlMenu extends Component{
 
   render(){
     var enable = false;
-    var space = true;
+    var space = false;
     return (
-        <Row>
-          <Col md="4">
-          </Col>
-          <Col md="4">
-            <ButtonGroup>
-              {enable && <Button color="secondary" onClick={this.props.onSetSidebarOpen}> <span className="float-right"><i className="icon-menu"></i></span></Button>}
-              <Button color="primary" onClick={() => this.props.onRadioBtnClick(1)} active={this.props.rSelected === 1}>Control</Button>
-              {space && <Col md="2"/>}
-              <Button color="primary" onClick={() => this.props.onRadioBtnClick(2)} active={this.props.rSelected === 2}>Comparison</Button>
-              {space && <Col md="2"/>}
-              <Button color="primary" onClick={() => this.props.onRadioBtnClick(3)} active={this.props.rSelected === 3}>Analytics</Button>
-            </ButtonGroup>
-          </Col>
-          <Col md="4">
-          </Col>
-        </Row>
+          <Row>
+            {enable && <Button color="secondary" onClick={this.props.onSetSidebarOpen}> <span className="float-right"><i className="icon-menu"></i></span></Button>}
+            <Col md="4">
+              <Button primary color="primary" onClick={() => this.props.onRadioBtnClick(1)} active={this.props.rSelected === 1} block>Control</Button>
+            </Col>
+            <Col md="4">
+              <Button primary color="primary" onClick={() => this.props.onRadioBtnClick(2)} active={this.props.rSelected === 2} block>Comparison</Button>
+            </Col>
+            <Col md="4">
+              <Button primary color="primary" onClick={() => this.props.onRadioBtnClick(3)} active={this.props.rSelected === 3} block>Analytics</Button>
+            </Col>
+          </Row>
     )
   }
 
